775d4de2b47b6d9ff3db527eb23a4cac0e27c91c
Hitarth Sharma <hitarth.sharma@outlook.com>
2019-07-03 21:51:25 +1200

    Initial commit

---
+++ HitarthShoppingCartAPI/HitarthShoppingCartAPI/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using HitarthShoppingCartAPI.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HitarthShoppingCartAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class ProductsController : ControllerBase
+  {
+    // GET api/products
+    [HttpGet]
+    public ActionResult<IEnumerable<ProductDto>> Get()
+    {
+      return new ProductDto[] {
+        new ProductDto{
+          Id = "1",
+          Name = "Coffee",
+          Price = 5.50m
+        },
+        new ProductDto {
+          Id = "2",
+          Name = "Milk",
+          Price = 7.50m
+        },
+        new ProductDto {
+          Id = "3",
+          Name = "Sugar",
+          Price = 4
+        },
+        new ProductDto {
+          Id = "4",
+          Name = "Cookie",
+          Price = 17
+        },
+        new ProductDto {
+          Id = "5",
+          Name = "Chocolate",
+          Price = 2.50m
+        }
+      };
+    }
+
+    // POST api/products
+    [HttpPost]
+    public void Post([FromBody] List<ProductDto> products)
+    {
+    }
+  }
+}
